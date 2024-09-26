af85ae6d0b42976909d1820471bf753356b9a265
webaldar <webaldar@gmail.com>
2024-09-26 14:50:16 +0300

    refactor Rating component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Switcher} from "./component/switcher";
@@ -7,13 +7,17 @@ import {UncontrolledRating} from "./component/UncontrolledRating";
 import {Accordion} from "./component/Accordion";
 import {UncontrolledAccordion} from "./component/UncontrolledAccordion";
 
+export type ratingValueType = 0 | 1 | 2 | 3 | 4 | 5
 function App() {
+
+
+    const [rating, setRating] = useState<ratingValueType>(0)
     return (
         <div className="App">
-            <Rating value={4}/>
             <UncontrolledRating/>
             <Switcher/>
             <Accordion title={'Menu'} collapsed={false}/>
+            <Rating value={rating} ratingChange={setRating}/>
             <UncontrolledAccordion title={'Menu uncontrolled'} />
 
         </div>
--- src/component/Rating.tsx
+++ src/component/Rating.tsx
@@ -1,27 +1,32 @@
 // @flow
 import * as React from 'react';
 import './Rating.css'
+import {ratingValueType} from "../App";
 
 type StarType = {
-    value: number
+    value: ratingValueType
+    ratingChange: (value: ratingValueType) => void
 };
 export const Rating = (props: StarType) => {
     return (
         <div>
-            <Star selected={props.value > 0}/>
-            <Star selected={props.value > 1}/>
-            <Star selected={props.value > 2}/>
-            <Star selected={props.value > 3}/>
-            <Star selected={props.value > 4}/>
+            <Star selected={props.value > 0} value={1} onClick={props.ratingChange}/>
+            <Star selected={props.value > 1} value={2} onClick={props.ratingChange}/>
+            <Star selected={props.value > 2} value={3} onClick={props.ratingChange}/>
+            <Star selected={props.value > 3} value={4} onClick={props.ratingChange}/>
+            <Star selected={props.value > 4} value={5} onClick={props.ratingChange}/>
         </div>
     );
 
 };
 type StarProps = {
     selected: boolean
+    value: ratingValueType
+    onClick: (value: ratingValueType) => void
 }
 function Star(props: StarProps) {
+
     // return props.selected ? <span><b>Star</b></span> : <span>Star</span>
 
-    return <span>{ props.selected ? <b>Star </b> : "Star "}</span>
+    return <span onClick={() =>{props.onClick(props.value)}}>{ props.selected ? <b>Star </b> : "Star "}</span>
 }
